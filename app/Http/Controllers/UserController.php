387a6029ae8b81cd19981eda735afad08a630d67
charles017 <charlie.abuloc@myyahoo.com>
2023-09-25 16:31:19 +0800

    UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register() {
+        return 'Hello!, from register function';
+    }
+}
