2cc7fd2b5c9c073ad8227048670b602fac9f3ede
charles017 <charlie.abuloc@myyahoo.com>
2023-09-25 16:31:33 +0800

    UserControls

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExampleController;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
